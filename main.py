c84f69a67b75f7ca6c31be8ab4e5d4ef6598ce0d
mrfrig <franklin@neosmail.com>
2025-06-15 15:10:13 -0400

    3-2 add first function declaration

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ def main():
         print("Usage: python3 main.py <prompt> [flags]")
         return sys.exit(1)
 
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     user_prompt = sys.argv[1]
     flags = sys.argv[2:]
 
@@ -27,10 +47,21 @@ def main():
     generated_content = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
     )
 
-    print(generated_content.text)
+    if len(generated_content.function_calls) > 0:
+        calls = []
+        for function_call_part in generated_content.function_calls:
+            calls.append(
+                f"Calling function: {function_call_part.name}({function_call_part.args})"
+            )
+        print("\n".join(calls))
+    else:
+        print(generated_content.text)
+
     if "--verbose" in flags:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
