52afdd80a97f1f14dd4807337f410190a6f8c9fa
mrfrig <franklin@neosmail.com>
2025-06-15 16:36:42 -0400

    3-4 add call function functionality

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import sys
 import os
+import sys
+
+from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from dotenv import load_dotenv
 
+from call_function import available_functions, call_function
 from prompts import system_prompt
-from call_function import available_functions
 
 
 def main():
@@ -51,7 +52,11 @@ def generate_content(client, messages, verbose):
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part)
+        if not hasattr(function_call_result.parts[0].function_response, "response"):
+            raise Exception("Response missing")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 
 
 if __name__ == "__main__":
