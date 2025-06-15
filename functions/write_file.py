be4114eabecfa0b9bbdf62c5cdbd3f30fc43b6d7
mrfrig <franklin@neosmail.com>
2025-06-15 15:20:21 -0400

    update functions to solution files

--- functions/write_file.py
+++ functions/write_file.py
@@ -3,18 +3,21 @@ import os
 
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
-    target = abs_working_dir
-    if file_path:
-        target = os.path.abspath(os.path.join(working_directory, file_path))
-    if not target.startswith(abs_working_dir):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f"Error: creating directory: {e}"
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
     try:
-        with open(target, "w") as f:
+        with open(abs_file_path, "w") as f:
             f.write(content)
-
         return (
             f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
         )
     except Exception as e:
-        return f"Error: {e}"
+        return f"Error: writing to file: {e}"
